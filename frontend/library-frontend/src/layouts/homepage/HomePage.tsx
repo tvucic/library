331133f8ff30c5104489af6209934426d9d7b64a
tvucic <tomo.vucic@gmail.com>
2023-06-10 13:25:14 +0200

    NO_TICKET .gitignore to root project changed

--- frontend/library-frontend/src/layouts/homepage/HomePage.tsx
+++ frontend/library-frontend/src/layouts/homepage/HomePage.tsx
@@ -3,7 +3,7 @@ import { Carousel } from './components/Carousel'
 import { ExploreTopBooks } from './components/ExploreTopBooks'
 import { Heros } from './components/Heros'
 import { LibraryServices } from './components/LibraryServices'
-
+//test comment
 export const HomePage = () => {
   return (
     <>
